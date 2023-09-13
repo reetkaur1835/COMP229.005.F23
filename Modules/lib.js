f8e61a7dd9a4b51c01dc54e20884b50320f2822f
reetkaur1835 <144295525+reetkaur1835@users.noreply.github.com>
2023-09-13 19:27:50 -0400

    adds classes and modules

---
+++ Modules/lib.js
@@ -0,0 +1,7 @@
+function halfOf(x){
+    return x/2;
+}
+
+function multiply(x,y){
+    return x * y;
+}
---
+++ Modules/main.js
@@ -0,0 +1,4 @@
+import {halfOf, multiply} from "./lib.js";
+
+console.log("halfOf: " + halfOf(84));
+console.log("multiply: ")
